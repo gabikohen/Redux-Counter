a99169a667e7f4835dd1cf39c5c7742e65e9cd93
gabikohen <gabokohen@gmail.com>
2022-01-02 17:55:18 -0300

    Redux counter

---
+++ src/components/counter/index.js
@@ -0,0 +1,24 @@
+import {deposit,withdraw} from "../../store/amount/action";
+import { selectCurrentAmount } from "../../store/amount/reducer";
+import {connect} from "react-redux"
+
+
+const mapStateProps = (state) => {
+    return{
+        amount:selectCurrentAmount(state),
+    }
+    
+}
+const counter = ({amount,deposit,withdraw}) => {
+  return (
+    <div>
+      <h1>{amount} </h1>
+       {/* tengo que poner la funcion ()=> funcion anonima  */}
+      <button onClick = {()=> deposit()}>Deposit $10</button>
+      <button onClick = {() => withdraw()}>Withdraw $10</button>
+    </div>
+  );
+};
+
+
+export default connect(mapStateProps,{deposit,withdraw})(counter)
