7b0539f1f6f791de83c52579ab5d0c465166003f
gabikohen <84341425+gabikohen@users.noreply.github.com>
2022-01-03 21:59:18 -0300

    Update index.js

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -13,7 +13,9 @@ const counter = ({amount,deposit,withdraw}) => {
   return (
     <div>
       <h1>{amount} </h1>
-       {/* tengo que poner la funcion ()=> funcion anonima  */}
+       {/* tengo que poner la funcion ()=> funcion anonima 
+       El primer parametro es para rescar algo y el segundoe enviar
+      connect()*/}
       <button onClick = {()=> deposit()}>Deposit $10</button>
       <button onClick = {() => withdraw()}>Withdraw $10</button>
     </div>
